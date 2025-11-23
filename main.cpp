c844d2b254134a3d8aada314688836a3eceb00fd
Your Name <WiegerWolf@hotmail.com>
2025-11-23 01:25:51 +0100

    update .gitignore, enhance CMake presets, and initialize cURL in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,7 @@
+#include <curl/curl.h>
+
 int main(){
+    // see https://everything.curl.dev/libcurl/globalinit.html
+    CURLcode curlInitResult = curl_global_init(CURL_GLOBAL_DEFAULT);
     return 0;
 }
