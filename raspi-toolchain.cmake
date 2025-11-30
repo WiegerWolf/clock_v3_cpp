385076f6913b86e4383bf16f79b6b9fd62436be3
Your Name <WiegerWolf@hotmail.com>
2025-11-30 16:48:42 +0100

    build-on-pi

--- raspi-toolchain.cmake
+++ raspi-toolchain.cmake
@@ -1,19 +1,30 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR arm)
 
-set(CMAKE_C_COMPILER arm-linux-gnueabihf-gcc)
-set(CMAKE_CXX_COMPILER arm-linux-gnueabihf-g++)
+set(CMAKE_C_COMPILER armv7-rpi2-linux-gnueabihf-gcc)
+set(CMAKE_CXX_COMPILER armv7-rpi2-linux-gnueabihf-g++)
 
 get_filename_component(MY_SYSROOT "${CMAKE_CURRENT_LIST_DIR}/pi_sysroot" ABSOLUTE)
+set(CMAKE_SYSROOT ${MY_SYSROOT})
 set(SYSROOT_FLAGS "--sysroot=${MY_SYSROOT} -isystem ${MY_SYSROOT}/usr/include -isystem ${MY_SYSROOT}/usr/include/arm-linux-gnueabihf")
+set(ARCH_FLAGS "-mcpu=cortex-a7 -mfpu=neon-vfpv4 -mfloat-abi=hard")
 
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${SYSROOT_FLAGS}")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${SYSROOT_FLAGS}")
-set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} --sysroot=${MY_SYSROOT}")
+set(CMAKE_SYSROOT_COMPILE ${MY_SYSROOT})
+set(CMAKE_SYSROOT_LINK ${MY_SYSROOT})
 
-# Tell CMake to look for programs on the Host, but Libs/Headers in the Sysroot
+set(CMAKE_LIBRARY_ARCHITECTURE arm-linux-gnueabihf)
+
+set(CMAKE_C_FLAGS "${ARCH_FLAGS} ${CMAKE_C_FLAGS}" CACHE STRING "" FORCE)
+set(CMAKE_CXX_FLAGS "${ARCH_FLAGS} ${CMAKE_CXX_FLAGS}" CACHE STRING "" FORCE)
+set(CMAKE_EXE_LINKER_FLAGS "--sysroot=${MY_SYSROOT}")
+
 set(CMAKE_FIND_ROOT_PATH ${CMAKE_SYSROOT})
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+
+set(PKG_CONFIG_USE_CMAKE_PREFIX_PATH ON)
+set(ENV{PKG_CONFIG_DIR} "")
+set(ENV{PKG_CONFIG_LIBDIR} "${CMAKE_SYSROOT}/usr/lib/pkgconfig:${CMAKE_SYSROOT}/usr/lib/arm-linux-gnueabihf/pkgconfig:${CMAKE_SYSROOT}/usr/share/pkgconfig")
+set(ENV{PKG_CONFIG_SYSROOT_DIR} ${CMAKE_SYSROOT})
